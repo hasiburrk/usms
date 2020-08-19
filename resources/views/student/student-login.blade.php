5b7b53353ff6f2d9629ff96e6daae3b46c2ffe2b
Animesh Karmakar <animesh.pust@gmail.com>
2020-08-19 20:14:27 +0600

    student login page added

--- resources/views/student/student-login.blade.php
+++ resources/views/student/student-login.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="/admin/assets/css/bootstrap.min.css">
   </head>
   <body>
+
     <div class="container py-5 mt-5 shadow-lg" style="border: 1px solid black; background: lightgray;">
       <div class="row justify-content-center">
         <h1 class="text-center">student login form</h1>
@@ -16,17 +17,20 @@
             @csrf
             <tr>
               <td>Enter Mobile number: </td>
-              <td><input type="text" name = 'mobile' id="mobile"></td>
+            <td><input type="text" name = 'mobile' id="mobile" value="{{ old('mobile') }}"></td>
               <td><span id="mobile_error"></span></td>
             </tr>
             <tr>
               <td>Enter Password: </td>
-              <td><input type="password" name = "password" id="password"></td>
+            <td><input type="password" name = "password" id="password"></td>
               <td><span id="pass_error"></span></td>
             </tr>
             <tr>
-              <td><input type="submit" name = "submit" id="submit" value="Login" class="btn btn-success"></td>
-              <td><p>Not a student?</p> <a href="/">Click here to Login</a></td>
+              <td></td>
+              <td><input type="submit" name = "submit" id="submit" value="Login" class="btn btn-success btn-block"></td>
+            </tr>
+            <tr class="mt-5" style="margin-top: 10px;">              
+              <td><p>Not a student?</p></td> <td><a href="/">Click here to Login</a></td>
             </tr>
           </form>
         </table>
